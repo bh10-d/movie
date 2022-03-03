1c0ae115c066b9009d30c3a9a741f24507a977ed
bh10-d <duchieubui25012002@gmail.com>
2022-03-03 07:22:37 +0700

    create-app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // fetch('https://ga-mobile-api.loklok.tv/cms/app').then(response => response.json())
+  // .then(data => console.log(data));
+
+  // fetch('https://ga-mobile-api.loklok.tv/cms/app/movieDrama/get?id=8084&category=0').then(response => response.json())
+  // .then(data => console.log(data));
+
+  const request = new Request('https://ga-mobile-api.loklok.tv/cms/app/movieDrama/get?id=8084&category=0');
+
+  // const url = request.url;
+  // const method = request.method;
+  // const credentials = request.credentials;
+  fetch(request)
+  .then(response => response.blob())
+  .then(blob => {
+    // image.src = URL.createObjectURL(blob);
+    console.log(blob);
+  });
+
+
   return (
     <div className="App">
       <header className="App-header">
