6fb23d4163b39cf16621fec49ad81ddd6d66fc38
bh10-d <duchieubui25012002@gmail.com>
2022-03-17 00:26:32 +0700

    --

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import useSWR from 'swr';
+import axios from 'axios'
 // import logo from './logo.svg';
 // import './App.css';
 import Navbar from './components/navbar/navbar.component';
@@ -10,25 +12,50 @@ const HomePage = () => {
   const [preview, setPreview] = useState([]);
   const [loading, setLoading] = useState(false);//false
 
+  // useEffect(() => {
+  //   fetch('https://ga-mobile-api.loklok.tv/cms/app/homePage/getHome?page=0', {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-type': 'application/json;charset=UTF-8',
+  //       'lang': 'en',
+  //       'versioncode': '11',
+  //       'clienttype': 'ios_jike_default'
+  //     }
+  //   }).then(res => res.json())
+  //     .then(d => {
+  //       setPreview(d.data.recommendItems);
+  //       // console.log(d.data.recommendItems);
+  //     })
+  //     .catch(err => console.log(err))
+  //     .finally(() => setLoading(false));
+  // }, []);
+
+
+
+
   useEffect(() => {
-    fetch('https://ga-mobile-api.loklok.tv/cms/app/homePage/getHome?page=0', {
-      method: 'GET',
+    getAllNotes () ;
+ }, []);
+  const url = "https://ga-mobile-api.loklok.tv/cms/app/homePage/getHome?page=0";
+  const getAllNotes = () => {
+    axios.get (`${url}`,{
       headers: {
-        'Content-type': 'application/json;charset=UTF-8',
-        'lang': 'en',
-        'versioncode': '11',
-        'clienttype': 'ios_jike_default'
+        lang: "en",
+        versioncode: "11",
+        clienttype: "ios_jike_default",
       }
-    }).then(res => res.json())
-      .then(d => {
-        setPreview(d.data.recommendItems);
-        console.log(d.data.recommendItems);
-      })
-      .catch(err => console.log(err))
-      .finally(() => setLoading(false));
-  }, []);
+    })
+     .then((response) => {
+      console.log(response.data.data.recommendItems);
+      setPreview(response.data.data.recommendItems);
+    } )
+     .catch (error => console.error (`Error: ${error}`));
+}
 
+  // const fetcher = url => axios.get(url).then(res => res.data)
+  // const {data, error} = useSWR('https://ga-mobile-api.loklok.tv/cms/app/homePage/getHome?page=0', fetcher)
 
+  // console.log(data)
 
   return (
     <div>
