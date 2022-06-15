ddfe0cbe9101255a389431e0dba2e211f85bc4f7
bh10-d <duchieubui25012002@gmail.com>
2022-06-15 10:56:19 +0700

    update feature delete history

--- src/pages/history/history.component.jsx
+++ src/pages/history/history.component.jsx
@@ -5,13 +5,15 @@ import Navbart from '../../components/navbar/navbart.component';
 import {AppContext} from '../../context/AppProvider';
 
 const History = ()=>{
-    const {history} = React.useContext(AppContext);
+    const {history, setHistory} = React.useContext(AppContext);
 
-    const handleClick = ()=>{
-        const newJobs = ["asd","alshjdkjh"]
-        const jsonJobs = JSON.stringify(newJobs)
-        localStorage.setItem('jobs',jsonJobs)
-        return newJobs
+    const handleClick = (id) => {
+        setHistory(()=>{
+            const deMovie = history.filter((movie)=> movie.id !== id)
+            const jsonMovie = JSON.stringify(deMovie)
+            localStorage.setItem('history',jsonMovie)
+            return deMovie
+        })
     }
 
     const resize = (url)=>{
@@ -27,15 +29,9 @@ const History = ()=>{
                 <h2 className="font-bold text-xl mt-4 mb-4">{(history.length != 0)?"Phim mà các cậu đã xem đấy":"Các cậu chưa xem phim nào cả"}</h2>
                 <div className="grid gap-2 grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 justify-items-center">
                     {history.map((m, i)=>(
-                        // <div key={i}>
-                        //     <div className="w-44 h-60 bg-slate-50 mr-3 text-slate-900 relative">
-                        //         <button className="right-0 text-slate-900 bg-slate-400 font-semibold absolute opacity-70" onClick={handleClick}>Xoá</button>
-                        //         <img src={m.imageUrl} alt="askjdh" />
-                        //     </div>
-                        //     <h1 className="text-slate-300 mt-3">{m.title}</h1>
-                        // </div>
-                        <Link className="md:w-[225px]" to={`/watch/${m.category}/${m.id}`} key={i}>
-                            <div className="block_item-movie hover:text-zinc-500 overflow-hidden">
+                        <div className="block_item-movie hover:text-zinc-500 overflow-hidden relative">
+                            <button className="right-0 text-slate-900 bg-slate-400 font-semibold absolute opacity-70 z-99" onClick={() =>{ handleClick(m.id)}}>Xoá</button>
+                            <Link className="md:w-[225px]" to={`/watch/${m.category}/${m.id}`} key={i}>
                                 <LazyLoadImage
                                     className="transition duration-700 object-cover h-[295px] w-[225px]"
                                     alt={m.title}
@@ -44,8 +40,8 @@ const History = ()=>{
                                     delayTime={1000}
                                 />
                                 <h1 className="text-ellipsis mb-3">{m.title}</h1>
-                            </div>
-                        </Link>
+                            </Link>
+                        </div>
                     ))}
                 </div>
             </div>
@@ -53,4 +49,4 @@ const History = ()=>{
     )
 }
 
-export default History;
+export default React.memo(History);
